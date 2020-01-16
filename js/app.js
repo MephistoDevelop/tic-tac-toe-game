23a28cf3ea3520ce60eda03eeeafd70093270ffe
ansaryergesh <yergeshovansar@gmail.com>
2020-01-17 02:09:03 +0600

    logic for display started

--- js/app.js
+++ js/app.js
@@ -38,7 +38,55 @@ const gameBoard = (() => {
     return {getBoard,resetBoard,};
  })();
  
-
+//display controller
 const displayController = (()=>{
+    let player1 = Player("Ansar", "X");
+    let player2 = Player("Memphisto", "O");
+    let countClicks = clickCounter();
+    let counter = 0;
 
+    const clickCounter = () => {
+        let counter = 0;
+        return() => {
+            counter++;
+            return counter;
+        }
+    };
+
+    const resetClicks = () => {
+        countClicks = clickCounter();
+        counter = 0;
+        switchTurn(counter);
+    };
+
+    const playGame = () => {
+        let boxCells = document.querySelector('.box');
+        for (let boxCell of boxCells) {
+            boxCells.addEventListener('click', markEachBoard);
+        }
+    }
+
+    const switchTurn = () => {
+        let message = document.getElementById("messages");
+        if (counter % 2 == 0){
+            message.innerText = `${player1.getName()}'s Turn`;
+        }else {
+            message.innerText = `${player2.getName()}'s Turn`;
+        }
+    }
+
+
+
+    const markEachBoard = () => {
+
+    };
+
+    const removeEventMark = () => {
+        let boxCells = document.querySelector('.box');
+        for(let boxCell of boxCells) {
+            boxCell.removeEventListener('click', markEachBoard);
+        }
+    }
+
+    return {clickCounter,resetClicks,playGame,switchTurn,markEachBoard,removeEventMark,};
 })
