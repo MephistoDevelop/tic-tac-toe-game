a443b9ea3e4aa68ba266b961904a54a1c3eec6d2
ansaryergesh <yergeshovansar@gmail.com>
2020-01-17 00:01:44 +0600

    player module

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
-import * as Board from './board.js';
+// import * as Board from './board.js';
 
-() => {
-  console.log("I'm main");
-};
+// () => {
+//   console.log("I'm main");
+// };
+//Player module
+
+const Player = (name, mark) => {
+    const getName = () => name;
+    const getMark = () => mark;
+    const addMark = (position) => {
+        gameBoard.getBoard()[position] = mark;
+        let boxPosition = document.getElementById(`${position}`);
+        boxPosition.textContent =gameBoard.getBoard()[position];
+    }
+
+    return {getName,getMark,addMark,};
+}
