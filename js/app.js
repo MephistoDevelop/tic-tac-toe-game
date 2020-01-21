d594da6a936c8daf9b05afb58f0058dac0836484
MephistoDevelop <inesherald@gmail.com>
2020-01-21 09:33:23 -0600

    Lintern Fix Blank Lines

--- js/app.js
+++ js/app.js
@@ -40,6 +40,28 @@ const displayController = (() => {
   const p2 = document.getElementById('player-2');
   let counter = 0;
   let endgame = false;
+  const winning = (board, symbol) => {
+    const win = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    win.forEach((element) => {
+      if (
+        board[element[0]] === symbol
+        && board[element[1]] === symbol
+        && board[element[2]] === symbol
+      ) {
+        endgame = true;
+      }
+    });
+  };
 
   function clickCounter() {
     return () => {
@@ -83,31 +105,6 @@ const displayController = (() => {
         player2.addMark(positionBox);
       }
       counter = countClicks();
-
-
-
-      const winning = (board, symbol) => {
-        const win = [
-          [0, 1, 2],
-          [3, 4, 5],
-          [6, 7, 8],
-          [0, 3, 6],
-          [1, 4, 7],
-          [2, 5, 8],
-          [0, 4, 8],
-          [2, 4, 6],
-        ];
-
-        win.forEach((element) => {
-          if (
-            board[element[0]] === symbol
-            && board[element[1]] === symbol
-            && board[element[2]] === symbol
-          ) {
-            endgame = true;
-          }
-        });
-      };
 
       switchTurn(counter);
       const mark = counter % 2 === 0 ? player2.getMark() : player1.getMark();
