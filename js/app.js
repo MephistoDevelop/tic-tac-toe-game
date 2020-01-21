a8db3048b26d363b9eb4f75181749d7abe50f47e
MephistoDevelop <inesherald@gmail.com>
2020-01-21 09:26:17 -0600

    Fix lintern no-use-before-define

--- js/app.js
+++ js/app.js
@@ -38,6 +38,8 @@ const displayController = (() => {
   const boxCells = document.querySelectorAll('.box');
   const p1 = document.getElementById('player-1');
   const p2 = document.getElementById('player-2');
+  let counter = 0;
+  let endgame = false;
 
   function clickCounter() {
     return () => {
@@ -46,8 +48,6 @@ const displayController = (() => {
     };
   }
   const countClicks = clickCounter();
-  let counter = 0;
-  let endgame = false;
 
 
   function giveName() {
@@ -84,6 +84,31 @@ const displayController = (() => {
       }
       counter = countClicks();
 
+
+
+      const winning = (board, symbol) => {
+        const win = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6],
+        ];
+
+        win.forEach((element) => {
+          if (
+            board[element[0]] === symbol
+            && board[element[1]] === symbol
+            && board[element[2]] === symbol
+          ) {
+            endgame = true;
+          }
+        });
+      };
+
       switchTurn(counter);
       const mark = counter % 2 === 0 ? player2.getMark() : player1.getMark();
       winning(gameBoard.getBoard(), mark);
@@ -113,29 +138,6 @@ const displayController = (() => {
       boxCell.addEventListener('click', markEachBoard);
     }
   }
-
-  const winning = (board, symbol) => {
-    const win = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    win.forEach((element) => {
-      if (
-        board[element[0]] === symbol
-        && board[element[1]] === symbol
-        && board[element[2]] === symbol
-      ) {
-        endgame = true;
-      }
-    });
-  };
 
   function winMessage(name) {
     msg.innerText = `${name} is winner!`;
